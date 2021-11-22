8eedef31259ff0a5b88bd701a0dae077ebf2f978
manpreet115 <94834089+manpreet115@users.noreply.github.com>
2021-11-22 15:23:29 +0530

    Update logical.py

--- logical.py
+++ logical.py
@@ -1,25 +1,9 @@
-# i=1
-# while i<=10:
-#     if i<=3:
-#         print(i)
-#     elif i==10 or i==9:
-#         print(i)
-#     else:
-#         print("*")
-#     i+=1
-
-# num1=int(input("enter the number"))
-# i=1
-# while i<=10:
-#     num2=int(input("enter the your "))
-#     if i<=5:
-#         print(" congratulation")
-#         break
-#     elif i<=4:
-#         print("sorry try again you have"+i,"chance")
-#     elif i<=3:
-#         print("sorry try again you have 3 chanse")
-#     elif i<=2:
-#         print("sorry try again you have 2 chanse")
-#     else:
-#         print("sorry you have 1 chanse")
+i=1
+while i<=10:
+    if i<=3:
+        print(i)
+    elif i==10 or i==9:
+        print(i)
+    else:
+        print("*")
+    i+=1
